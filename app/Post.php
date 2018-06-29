73b8144242dcb57f05092c918f5258d42a9aa1e5
RudeGames <fs_boukhoulda@esi.dz>
2018-06-29 19:24:23 +0100

    Added SoftDelete to Post Model

--- app/Post.php
+++ app/Post.php
@@ -3,13 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Post extends Model
 {
 
+    use SoftDeletes;
+
     protected $fillable = [
         'title','content','featured','category_id'
     ];
+
+    protected $dates = ['deleted_at'];
 
     public function category(){
         return $this->belongsTo('App\Category');
