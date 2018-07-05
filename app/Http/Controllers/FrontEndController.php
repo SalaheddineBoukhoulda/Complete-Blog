7158d6212bc6ee510112b28540f2815111e16d0b
RudeGames <fs_boukhoulda@esi.dz>
2018-07-05 19:21:59 +0100

    Completed index.php page with the new theme

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -16,6 +16,10 @@ class FrontEndController extends Controller
                     ->with('categories',Category::take(4)->get())
                     ->with('first_post',Post::orderBy('created_at','desc')->first())
                     ->with('second_post',Post::orderBy('created_at','desc')->skip(1)->take(1)->get()->first())
-                    ->with('third_post',Post::orderBy('created_at','desc')->skip(2)->take(1)->get()->first());
+                    ->with('third_post',Post::orderBy('created_at','desc')->skip(2)->take(1)->get()->first())
+                    ->with('unity',Category::find(1))
+                    ->with('laravel',Category::find(2))
+                    ->with('mysql',Category::find(6))
+                    ->with('settings',Settings::first());
     }
 }
